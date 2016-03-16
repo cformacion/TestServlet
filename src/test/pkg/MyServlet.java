9a4c8cb6ece5927a5006b829e47077e05d1ce144
cformacion <c-formacion@neiu.edu>
2016-03-16 13:33:54 -0500

    Dialogue Servlet
    code for dialogue servlet

---
+++ src/test/pkg/MyServlet.java
@@ -0,0 +1,57 @@
+package test.pkg;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter printWriter = response.getWriter();
+		Scanner kbd = new Scanner(System.in);
+		System.out.println("Enter added text for "+request.getParameter("firstname"));
+		String added =  process(kbd.nextLine());
+		printWriter.print("Welcome ");
+		String firstname = request.getParameter("firstname").toLowerCase()+added;
+		printWriter.print(firstname);
+	}
+ 
+	protected String process(String name){
+		if (name.equals("@I"))
+			return " Yay!";
+		else
+			return ". boo.";
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
